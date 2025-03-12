4fdf5ad7a283c134181b8b632ab323ac030ab677
Irina Khromova <irina.khromova@jetbrains.com>
2025-03-12 13:15:19 +0200

    ae

--- Class1.cs
+++ Class1.cs
@@ -2,4 +2,13 @@
 
 public class Class1
 {
+    public int Add(int number1, int number2)
+    {
+        return number1 + number2;
+    }
+    
+    public int Sub(int number1, int number2)
+    {
+        return number1 - number2;
+    }
 }
---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using Xunit;
+
+namespace SimpleClassLibrary.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        // Arrange
+        var instance = new Class1();
+
+        // Act
+        var result = instance.ToString();
+
+        // Assert
+        Assert.False(result == "Hello World!");
+
+    }
+    
+    [Fact]
+    public void Test2()
+    {
+        // Arrange
+        var instance = new Class1();
+
+        // Act
+        var result = instance.Add(2,3);
+
+        // Assert
+        Assert.True(result == 5);
+
+    }
+}
